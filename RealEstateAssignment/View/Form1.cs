4da3218685bcd2614798dc3770f86d9fce4d9d9c
André Sturesson <as226ng@student.lnu.se>
2022-09-09 13:19:25 +0200

    Some frontend additions

--- RealEstateAssignment/View/Form1.cs
+++ RealEstateAssignment/View/Form1.cs
@@ -1,3 +1,6 @@
+using RealEstateAssignment.Appartment;
+using static Enums;
+
 namespace RealEstateAssignment
 {
     public partial class Form1 : Form
@@ -7,7 +10,8 @@ namespace RealEstateAssignment
         {
             InitializeComponent();
 
-            comboBox1.DataSource = Enum.GetValues(typeof(Countries));
+            comboBox1.DataSource = Enums.contries.GetValues(typeof(contries));
+            comboBox2.DataSource = Enums.types.GetValues(typeof(types));
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -68,6 +72,58 @@ namespace RealEstateAssignment
         }
 
         private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sizeLabel.Visible = false;
+            sizeTextBox.Visible = false;
+            sizesqmLabel.Visible = false;
+            roomsLabel.Visible = false;
+            roomsTextBox.Visible = false;
+            switch (comboBox2.SelectedItem.ToString())
+            {
+                case "Apartment":
+                    sizeLabel.Visible = true;
+                    sizeTextBox.Visible = true;
+                    sizesqmLabel.Visible = true;
+                    roomsLabel.Visible = true;
+                    roomsTextBox.Visible = true;
+                    break;
+                case "Villa":
+                    break;
+                case "Rental":
+                    break;
+                case "Tenement":
+                    break;
+                case "Shop":
+                    break;
+                case "Warehouse":
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sizeLabel_Click(object sender, EventArgs e)
         {
 
         }
